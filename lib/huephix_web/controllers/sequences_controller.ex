d913c2939d6b177c4eb9f0a0bd720716beb2b7d1
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2018-09-23 15:04:26 +0200

    Cleanup

--- lib/huephix_web/controllers/sequences_controller.ex
+++ lib/huephix_web/controllers/sequences_controller.ex
@@ -3,16 +3,6 @@ defmodule HuephixWeb.SequencesController do
     alias Huephix.{Repo, Sequence}
     alias HuephixWeb.SharedView.CommonView
 
-    # Reference
-    # user_path  GET     /users           HelloWeb.UserController :index
-    # user_path  GET     /users/:id/edit  HelloWeb.UserController :edit
-    # user_path  GET     /users/new       HelloWeb.UserController :new
-    # user_path  GET     /users/:id       HelloWeb.UserController :show
-    # user_path  POST    /users           HelloWeb.UserController :create
-    # user_path  PATCH   /users/:id       HelloWeb.UserController :update
-    #         PUT     /users/:id       HelloWeb.UserController :update
-    # user_path  DELETE  /users/:id       HelloWeb.UserController :delete
-
     def index(conn, _params) do
         render conn, "index.json", data: Repo.all(Sequence)
     end
